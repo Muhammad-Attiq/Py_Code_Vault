0febf898049ff5b13f076b03d0e23dd9f8afcbf3
Muhammad Attiq <muhammad.attiq.computer.engineer@gmail.com>
2025-10-18 22:05:43 -0700

    Delete Easy directory

--- Easy/Palindrome_Number.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        rev = str(x)[::-1]
-        y = str(x)
-        if rev == y:
-            return True
-        else:
-            return False
--- Easy/Two_Sum.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        i = 0
-        j = 0
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if i != j and nums[i] + nums[j] == target:
-                    return i, j
-        return None
